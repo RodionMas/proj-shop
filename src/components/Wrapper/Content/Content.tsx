0f3afd1b822091465e9cc953b114e00dab6bb074
RodionMas <rrssrssrrr1997@bk.ru>
2024-07-14 20:38:24 +0300

    fixed typescript version conflict error

--- src/components/Wrapper/Content/Content.tsx
+++ src/components/Wrapper/Content/Content.tsx
@@ -1,32 +1,30 @@
 import React, { Suspense } from "react";
 import PizzaBlock from "../PizzaBlock/PizzaBlock";
 import Menu from "../Menu/Menu";
-
 import style from "./Content.module.css";
-// import Pagination from "./Pagination/Pagination";
 import { useSelector } from "react-redux";
 import { selectContent } from "../../../cart/selectors";
 import { lazy } from 'react';
 
 const Pagination = lazy(() => import('./Pagination/Pagination'));
 const Content: React.FC = () => {
-  const {pizzas, status} = useSelector(selectContent)
+  const { pizzas, status } = useSelector(selectContent)
   return (
     <div className={style.content}>
-      {status === 'rejected' ? 
-      <div className={style.error}>Все ошибаются и наш сервер тоже =(</div> 
-      : 
-      <>
-      <div className={style.container}>
-        <Menu />
-        {pizzas && (
-          <PizzaBlock />
-        )}
-      </div>
-      <Suspense fallback={<h1>Pagination button</h1>}>
-      <Pagination />
-      </Suspense>
-      </>}
+      {status === 'rejected' ?
+        <div className={style.error}>Все ошибаются и наш сервер тоже =(</div>
+        :
+        <>
+          <div className={style.container}>
+            <Menu />
+            {pizzas && (
+              <PizzaBlock />
+            )}
+          </div>
+          <Suspense fallback={<h1>Pagination button</h1>}>
+            <Pagination />
+          </Suspense>
+        </>}
     </div>
   );
 };
